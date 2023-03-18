3540fe4078ae7980bfa0786773d96337230404e4
Youssofalwardany <98885312+Youssofalwardany@users.noreply.github.com>
2023-03-18 12:07:28 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-//
-//  main.cpp
-//  Part 2
-//
-//  Created by Youssof Alwardany on 15/03/2023.
-//
-
 #include <iostream>
 #include <bitset>
 #include <cmath>
@@ -16,7 +9,7 @@
 using namespace std;
 
 
-vector<string> splitStringByPlus(string inputStr) {
+vector<string> splitString(string inputStr) {
     vector<string> result;
     size_t pos = 0;
     string delimiter = "+";
@@ -56,93 +49,110 @@ vector<string> convert(vector<string> expression, int n)
 }
 
 
-void print(vector<string> function, int size)
+vector<int> print(vector<string> function, int size)
 {
     int numRows = pow(2, size);
     
-    int counter=0;
-    
-    vector<vector<string>> table(numRows, vector<string>(size)); //try BOOL for both if get errors
+    vector<vector<int>> table(numRows, vector<int>(size)); //try BOOL for both if get errors
     
     for (int i = 0; i < numRows; i++) {
-            int entry = i;
-            for (int j = 0; j < size; j++) {
-                table[i][j] = entry % 2;
-                
-                entry /= 2;
-            }
-        }
-    
-    int j=0;
-    vector<bool> x(size, numRows);
-    
-    for (int i = 0; i < numRows; i++)
-    {
-            while(j<size)
-            {
-                for(int k=0; k<function[i].size(); k++)
-                {
-                if(function[k]!="-")
-                    {
-                        if(function[k]==table[i][j] && x[i]!=false)
-                        {
-                            x[i]=true;
-                        }
-                        else
-                        {
-                            x[i]=false;
-                        }
-                        j++;
-                    }
-                }
-            }
-            
-        }
-    
-    
-    for (int i = 0; i < numRows; i++)
-    {
-            for (int j = 0; j < size; j++) {
-                cout << table[i][j] << " ";
-            }
-            if(x[i]==true)
-            {
-                cout<<"1";
-            }
-            else
-            {
-                cout<<"0";
-            }
-            cout << endl;
-        }
-    
-}
-
-    
-
-int main()
-    {
-    
-    string function;
-    vector<string> result = splitStringByPlus(function);
-    
-    set<char> numberOfVariables;
-    
-    for(int i=0; i<result.size(); i++)
-    {
-        for(int j=0; j<result[i].size(); i++)
-        {
-            if(result[i][j]!='!')
-            {
-                char z = result[i][j];
-                numberOfVariables.insert(z);
-            }
+        for (int j = size - 1; j >= 0; j--) {
+            table[i][j] = (i >> (size - j - 1)) & 1;
         }
     }
     
-    int size = numberOfVariables.size();
+    vector<bool> F(numRows, false);
     
-    print(convert(result, size), size);
+    vector<string> tableU;
     
-    return 0;
+    for (int i = 0; i < numRows; i++) {
+        string temp = "";
+        for (int j = 0; j < size; j++) {
+            temp += static_cast<char>(table[i][j] + 48);
+        }
+        tableU.push_back(temp);
+    }
+    
+    for(int i =0; i< numRows; i++)
+    {
+        for(int j =0; j<function.size(); j++)
+        {
+            int counter =0;
+            for(int x =0; x<size; x++)
+            {
+                if(tableU[i][x] == function[j][x] ||function[j][x] == '-')
+                {
+                    counter++;
+                    //cout << tableU[i][x] << "== " << function[j][x] << endl;
+                }//else
+                   //cout << tableU[i][x] << "!= " << function[j][x] << endl;
+            }
+            //cout << counter << endl;
+            if(counter ==  size){
+                F[i] = true;
+                break;
+            }
+        }
+    }
+    char c;
+    for(c = 'a'; c < 'a'+ size; c++)
+    {
+        cout << c << " ";
+    }
+    cout << "F"<< endl;
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < size; j++) {
+            cout << table[i][j] << " ";
+        }
+        if(F[i]==true)
+        {
+            cout<<"1";
+        }
+        else
+        {
+            cout<<"0";
+        }
+        cout << endl;
+    }
+    
+    vector<int> posLines;
+    vector<int> sopLines;
+    
+    //function to put the lines of sop or pos
+    for (int i=0;  i < numRows; i++)
+    {
+            if(F[i]==true)
+            {
+                sopLines.push_back(i);
+            }
+            else if(F[i]==false)
+            {
+                posLines.push_back(i);
+            }
+    }
+    
+    
+    int i=0;
+    vector<string> sopValues(sopLines.size());
+    vector<string> posValues(posLines.size());
+    
+    cout << "\nm:Σ(";
+    while (i<sopLines.size())
+        {
+            cout << sopLines[i] << ",";
+            i++;
+        }
+    i = 0;
+    cout << ")\nM:Π(";
+    while (i<posLines.size())
+        {
+            cout << posLines[i] << ",";
+            i++;
+        }
+    cout << ")\n";
+
+    
+    return sopLines;
 }
+
+
---
+++ truthtable.hpp
@@ -0,0 +1,10 @@
+#ifndef truthtable_hpp
+#define truthtable_hpp
+
+#include <stdio.h>
+
+vector<string> splitString(string inputStr);
+vector<string> convert(vector<string> expression, int n);
+vector<int> print(vector<string> function, int size);
+
+#endif /* truthtable_hpp */
